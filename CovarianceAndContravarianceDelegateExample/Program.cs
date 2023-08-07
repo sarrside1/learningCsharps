5b4376872474215d7762e8772364687bd96c197c
sarrside1 <140917461+sarrside1@users.noreply.github.com>
2023-08-07 15:22:21 +0000

    Initiall commit

---
+++ CovarianceAndContravarianceDelegateExample/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace CovarianceAndContravarianceDelagateExample
+{
+    //Variance delegate
+    delegate Car CarFactoryDel(int id, string name);
+    //Contravariance delegate
+    delegate void LogICECarDetailsDel(ICECar iCECar);
+    delegate void LogEVCarDetailsDel(EVCar eVCar);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIANCE DELEGATE
+
+            Console.WriteLine("VARIANCE DELEGATE BEGIN HERE");
+            Console.WriteLine(new string('*', "VARIANCE DELEGATE BEGIN HERE".Length));
+            CarFactoryDel carFactoryDel = CarFactory.ReturnICECar;
+            
+            Car iceCar = carFactoryDel(1, "Audi R8");
+
+            Console.WriteLine($"Object type: {iceCar.GetType()}");
+            Console.WriteLine($"Car details: {iceCar.GetCarDetails()}");
+
+            carFactoryDel = CarFactory.ReturnEVCar;
+            Car evCar = carFactoryDel(2, "Tesla Model-3");
+            Console.WriteLine();
+            Console.WriteLine($"Object type: {evCar.GetType()}");
+            Console.WriteLine($"Car details: {evCar.GetCarDetails()}");
+            //Console.ReadKey();
+
+            Console.WriteLine(new string('*', "VARIANCE DELEGATE END HERE".Length));
+            Console.WriteLine("VARIANCE DELEGATE END HERE");
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("CONTRAVARIANCE DELEGATE BEGIN HERE");
+            Console.WriteLine(new string('*', "CONTRAVARIANCE DELEGATE BEGIN HERE".Length));
+            //CONTRAVARIANCE DELEGATE
+            LogICECarDetailsDel logICECarDetailsDel = LogCarDetails;
+            logICECarDetailsDel(iceCar as ICECar);
+
+            LogEVCarDetailsDel logEVCarDetailsDel = LogCarDetails;
+            logEVCarDetailsDel(evCar as EVCar);
+            Console.WriteLine(new string('*', "CONTRAVARIANCE DELEGATE END HERE".Length));
+            Console.WriteLine("CONTRAVARIANCE DELEGATE END HERE");
+            Console.ReadKey();
+        }
+
+        static void LogCarDetails(Car car)
+        {
+            if(car is ICECar)
+            {
+                using(StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ICEDeatails.txt"), true))
+                {
+                    sw.WriteLine($"Object type: {car.GetType()}");
+                    sw.WriteLine($"Car details: {car.GetCarDetails()}");
+                }
+            }
+            else if(car is EVCar)
+            {
+                Console.WriteLine($"Object type: {car.GetType()}");
+                Console.WriteLine($"Car details: {car.GetCarDetails()}");
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+
+    public static class CarFactory
+    {
+        public static ICECar ReturnICECar(int id, string name)
+        {
+            return new ICECar { Id = id, Name = name };
+        }
+        public static EVCar ReturnEVCar(int id, string name)
+        {
+            return new EVCar { Id = id, Name = name };
+        }
+    }
+
+    public abstract class  Car
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual string GetCarDetails()
+        {
+            return $"{Id} - {Name}";
+        }
+    }
+
+    public class ICECar: Car
+    {
+        public override string GetCarDetails()
+        {
+            return $"{base.GetCarDetails()} - Internal Combustion Engine";
+        }
+    }
+
+    public class EVCar : Car
+    {
+        public override string GetCarDetails()
+        {
+            return $"{base.GetCarDetails()} - Electric";
+        }
+    }
+}
---
+++ FuncActionPredicateExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FuncActionPredicateExamplels
+{
+    class Program
+    {
+        delegate TResult Func2<out TResult>();
+        delegate TResult Func2<in T1, out TResult>(T1 arg);
+        delegate TResult Func2<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+        delegate TResult Func2<in T1, in T2, in T3, out TResult>(T1 arg1, T2 arg2, T3 arg3);
+        static void Main(string[] args)
+        {
+            MathClass mathClass = new MathClass();
+
+            Func2<int, int, int> calc = mathClass.Sum;
+            Func<int, int, int> calc1 = delegate (int a, int b) { return a + b; };
+            Func<int, int, int> calc2 = (int a, int b) =>  a + b;
+            int result = calc(1, 2);
+            int result2 = calc(1, 2);
+            Console.WriteLine($"Result1: {result}");
+            Console.WriteLine($"Result2: {result2}");
+            Console.ReadKey();
+        }
+    }
+
+    public class MathClass
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
